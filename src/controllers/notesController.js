d2f70bc2a1cddcb6b6218b1b93add0677a45fe60
BielMR2 <gabrielmribeiro2007@outlook.com>
2023-12-14 14:28:14 -0300

    notes check

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,36 +2,44 @@ const knex = require("../database/knex")
 
 class NotesController{
     async create(request, response){
-        const { title, description, tags, links } = request.body
-        const user_id = request.user.id
-
-        const [note_id] = await knex("notes").insert({
-            title,
-            description,
-            user_id
-        });
-        
-        const linksInsert = links.map(link => {
-            return {
-                note_id,
-                url: link
-            }
-        });
- 
-        await knex("links").insert(linksInsert)
-
-        const tagsInsert = tags.map(name => {
-            return {
-                note_id,
-                name,
+        try {
+            const { title, description, tags, links } = request.body;
+            const user_id = request.user.id;
+    
+            const [note_id] = await knex("notes").insert({
+                title,
+                description,
                 user_id
+            });
+    
+            if (links && links.length > 0) {
+                const linksInsert = links.map(link => {
+                    return {
+                        note_id,
+                        url: link
+                    };
+                });
+    
+                await knex("links").insert(linksInsert);
             }
-        });
-
-        await knex("tags").insert(tagsInsert)
-
-
-        return response.json()
+    
+            if (tags && tags.length > 0) {
+                const tagsInsert = tags.map(name => {
+                    return {
+                        note_id,
+                        name,
+                        user_id
+                    };
+                });
+    
+                await knex("tags").insert(tagsInsert);
+            }
+    
+            return response.json({ success: true });
+        } catch (error) {
+            console.error("Erro durante a criação da nota:", error);
+            return response.status(500).json({ error: "Erro interno do servidor" });
+        }
     }
 
     async show(request, response){
